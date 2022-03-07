528fd419858e7b1dbca71795943a378648f1c1be
Andrew Watts-Curnow <andywatts@yahoo.com>
2022-03-07 07:58:39 -0500

    Runtime hierarchy build throws overflow when Rotate added to parent

--- Assets/CreateHierachy.cs
+++ Assets/CreateHierachy.cs
@@ -16,20 +16,28 @@ public class CreateHierachy : SystemBase
         {
             Debug.Log("Create parent and cube");
             
-            var cubePrefab = GetSingleton<CubePrefab>().Value;
-            var cube = EntityManager.Instantiate(cubePrefab);
-            
             var parentPrefab = GetSingleton<ParentPrefab>().Value;
             var parent = EntityManager.Instantiate(parentPrefab);
             
+            var cubePrefab = GetSingleton<CubePrefab>().Value;
+            var cube = EntityManager.Instantiate(cubePrefab);
             EntityManager.AddComponentData(cube, new Parent {Value = cube});
-            EntityManager.AddComponentData(cube, new LocalToParent() {});
+            EntityManager.AddComponentData(cube, new LocalToParent {Value = new float4x4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)});
+                
+                
             var children = EntityManager.AddBuffer<Child>(parent);
             children.Add(new Child {Value = cube});
 
             var q = quaternion.EulerXYZ(0, 45 * Mathf.Deg2Rad, 0);
             SetComponent(parent, new Rotation {Value = q});
             runOnce = true;
+            
+            
+            // BUGS
+            // No rotation unless LocalToParent
+            // stackOverflow with LocalToParent
+            
+            
         }
 
     }
